face1a069789c8f46ff0da3e1699421644c2cb71
Kunal Kumar Shah <kunalkumarshah@gmail.com>
2018-04-10 15:26:31 +0530

    Initial Commit

--- DataClean/HandleMissingValues.py
+++ DataClean/HandleMissingValues.py
@@ -20,13 +20,14 @@ total_cells = np.product(bldg_permits.shape)
 total_missing = missing_values_count.sum()
 print((total_missing/total_cells) * 100)
 
+# Drop the rows having atleast 1 column with missing value
+print(bldg_permits.dropna())
+
 # Drop the columns which have atleast 1 missing value
 columns_with_na_dropped = bldg_permits.dropna(axis=1)
 columns_with_na_dropped.head()
 print("Columns in original dataset: %d \n" % bldg_permits.shape[1])
 print("Columns with na's dropped: %d" % columns_with_na_dropped.shape[1])
 
-# backfill the columns missing values with the next column value and last column as 0
-bldg_permits.fillna(method = 'bfill', axis=0).fillna("0")
-
-print(bldg_permits.sample(5))
+# backfill the columns missing values with the next Row-Column value and last row-column as 0
+print(bldg_permits.fillna(method = 'bfill', axis=0).fillna("0").head())
--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 =======
 # Python
 >>>>>>> 7acfe3bf2fc659ebed8b7cf9e801c37dc6606880
+
+This Repository contains Sample Code created using Python
